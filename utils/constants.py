f1d210952ad74b24644c27c857976b656d34b7d8
emmaleah-shp <110855997+emmaleah-shp@users.noreply.github.com>
2026-01-27 14:41:00 -0300

    Add 'a3' to MODEL_ITERS constant

--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,4 @@
-MODEL_ITERS = ["a1", "a2", "b1", "b1_pruned"]
+MODEL_ITERS = ["a1", "a2", "a3", "b1", "b1_pruned"]
 MODELS = ["rf", "xgb", "lgbm", "cat"]
 USES = ["residential", "commercial", "office"]
+
