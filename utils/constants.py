63e46b499060d1344685d09cc1194f1db329afa6
emmaleah-shp <110855997+emmaleah-shp@users.noreply.github.com>
2026-01-29 15:55:12 -0300

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,5 @@
-MODEL_ITERS = ["a1", "a2", "a3", "b1", "b1_pruned"]
-MODELS = ["rf", "xgb", "lgbm", "cat"]
+MODEL_ITERS = ["a1", "a2", "a3", "b1", "b1_pruned", "c1", "c2"]
+MODELS = ["rf", "xgb", "lgbm", "cat", "nn", "nn_log"]
 USES = ["residential", "commercial", "office"]
+
 
