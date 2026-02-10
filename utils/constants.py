17b57af123338bb018f2dae12ec54ba8a74a9399
emmaleah-shp <110855997+emmaleah-shp@users.noreply.github.com>
2026-02-10 18:08:11 -0300

    Update MODEL_ITERS with additional entries

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,6 @@
-MODEL_ITERS = ["a1", "a2", "a3", "b1", "b1_pruned", "c1", "c2"]
+MODEL_ITERS = ["a1", "a2", "a3", "a1_b2", "a2_b2", "a3_b2", "a4_b2", "a1_b2_pruned", "a2_b2_pruned", "a3_b2_pruned", "a4_b2_pruned", "a1_c1", "a2_c1", "a3_c1", "a4_c1"]
 MODELS = ["rf", "xgb", "lgbm", "cat", "nn", "nn_log"]
 USES = ["residential", "commercial", "office"]
+
 
 
