042f2194090995428f9e1e9d34a52c9a4ba09fc4
emmaleah-shp <110855997+emmaleah-shp@users.noreply.github.com>
2026-03-12 14:56:48 -0300

    Refactor data loading functions to use scale parameter

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,12 +3,8 @@ import pandas as pd
 import streamlit as st
 
 @st.cache_data
-def load_h9_gdf(place):
-    return gpd.read_parquet(f"data/error_results_{place}_h9.geoparquet")
-
-@st.cache_data
-def load_h10_gdf(place):
-    return gpd.read_parquet(f"data/error_results_{place}_h10.geoparquet")
+def load_gdf(place, scale):
+    return gpd.read_parquet(f"data/error_results_{place}_{scale}.geoparquet")
 
 @st.cache_data
 def load_metrics():
@@ -27,6 +23,7 @@ def load_feature_summary():
     # df = df.iloc[:, 1:7]
 
     return df
+
 
 
 
