2e43b8dcf508c5e261c3c49c1ae8f6d2a3da411a
emmaleah-shp <110855997+emmaleah-shp@users.noreply.github.com>
2026-03-12 14:49:55 -0300

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,7 @@
-MODEL_ITERS = ["a1", "a2", "a3", "a1_b2", "a2_b2", "a3_b2", "a4_b2", "a1_b2_pruned", "a2_b2_pruned", "a3_b2_pruned", "a4_b2_pruned", "a1_c1", "a2_c1", "a3_c1", "a4_c1"]
-MODELS = ["rf", "xgb", "lgbm", "cat", "nn", "nn_log"]
-USES = ["residential", "commercial", "office"]
+MODEL_ITERS = ["a1", "a2", "a3", "a4"]
+CITIES = ["temuco", "losangeles", "osorno", "valdivia", "talca", "puertovaras", "curico", "pucon", "medellin"]
+USES = ["residential", "commercial", "office", "commercial_office"]
+
 
 
 
