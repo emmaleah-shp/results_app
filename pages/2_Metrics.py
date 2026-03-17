45c2c4d79f57502564aba3083afc1a7e9e9096d4
emmaleah-shp <110855997+emmaleah-shp@users.noreply.github.com>
2026-03-17 12:56:15 -0300

    Update 2_Metrics.py

--- pages/2_Metrics.py
+++ pages/2_Metrics.py
@@ -6,56 +6,48 @@ metrics = load_metrics()
 
 st.title("Model Performance Metrics")
 
-use = st.selectbox("Uso del suelo", sorted(metrics["use"].unique()))
-model = st.multiselect(
-    "Modelo",
-    sorted(metrics["model"].unique()),
-    default=sorted(metrics["model"].unique())
-)
-
-if model == "rf":
-    model_name = "Random Forest"
-elif model == "xgb":
-    model_name = "XGBoost"
-elif model == "lgbm":
-    model_name = "LightGBM"
-elif model == "cat":
-    model_name = "CatBoost"
-elif model == "nn" or model == "nn_log":
-    model_name = "Multilayer Perceptron (red neuronal)"
-
+use = st.selectbox("Uso del suelo", sorted(metrics["land_use"].unique()))
 
 iteration = st.multiselect(
     "Iteración",
-    sorted(metrics["model_iteration"].unique()),
-    default=sorted(metrics["model_iteration"].unique())
+    sorted(metrics["iteration"].unique()),
+    default=sorted(metrics["iteration"].unique())
 )
 
 df = metrics[
-    (metrics["use"] == use) &
-    (metrics["model"].isin(model)) &
-    (metrics["model_iteration"].isin(iteration))
+    (metrics["land_use"] == use) &
+    (metrics["iteration"].isin(iteration))
 ]
 
-st.dataframe(df.sort_values("MAE"), use_container_width=True)
+st.dataframe(df.sort_values("mae"), use_container_width=True)
 
-barshow = st.selectbox("Ver estadística", ["R2","MAE"])
-if barshow =="MAE": 
+barshow = st.selectbox("Ver estadística", ["Log R2", "R2","MAE"])
+if barshow =="Log R2": 
     fig = px.bar(
         df,
-        x="model",
-        y="MAE",
-        color="model_iteration",
+        x="use",
+        y="log_r2",
+        color="iteration",
+        barmode="group",
+        title="Log R2 by Model and Iteration",
+    )
+    
+elif barshow =="MAE": 
+    fig = px.bar(
+        df,
+        x="use",
+        y="mae",
+        color="iteration",
         barmode="group",
         title="MAE by Model and Iteration",
     )
     
-else: 
+elif barshow =="R2": 
     fig = px.bar(
         df,
-        x="model",
-        y="R2",
-        color="model_iteration",
+        x="use",
+        y="r2",
+        color="iteration",
         barmode="group",
         title="R2 by Model and Iteration",
     )
